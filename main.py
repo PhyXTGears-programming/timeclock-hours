3c3f9a49af6ab721cd4c730509b63eb014ce71dc
PiGuy2 <rsamm3@gmail.com>
2017-10-18 20:46:19 -0400

    start on calculating time

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from tkinter import filedialog
-# from tkinter import ttk
-# from tkinter.filedialog import askopenfilename
+
+ioForm = "%H:%M:%S %d.%m.%Y"
+
+
+def calcTotalTime(n):  # returns total time in seconds
+    total = 0
+    iLin, oLin = '', ''
+    prev = 'n'
+    for line in open(opts['pathTime'] + n + '.txt'):
+        line = line.strip()
+        lastIOA = prev[0]
+        currIOA = line[0]
+
+        if currIOA == 'i':
+            iLin = line[4:]
+        elif currIOA == 'o' and lastIOA != 'o' and iLin != '':
+            oLin = line[4:]
+            total = total + (datetime.strptime(oLin, opts['ioForm']) - datetime.strptime(iLin, opts['ioForm'])).total_seconds()
+        prev = line
+    return total
+
 
 if __name__ == "__main__":
     # root = Tk()
@@ -11,6 +30,7 @@ if __name__ == "__main__":
         for f in filenames:
             with open(os.path.join(root, f), 'r') as userFile:
                 state = "o"
-                lastTime = 
+                # lastTime =
                 for l in userFile:
-                    #
+                    time_str = line[4:]
+                    time = datetime.strptime(time_str, ioForm)
