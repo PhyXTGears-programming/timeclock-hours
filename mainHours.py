fdd2b9e6c6a7d3e7d352cba0db78d8abbd96a557
Robby Sammelson <rsamm3@gmail.com>
2017-10-20 22:24:27 -0400

    Fixes to time calculation

--- mainHours.py
+++ mainHours.py
@@ -1,3 +1,4 @@
+import math
 import os
 from datetime import datetime
 from tkinter import filedialog
@@ -30,13 +31,13 @@ def getSecs(fil):
 
 def formatTime(secs):  # 2 lines
     time_str = str(math.floor(secs / (24.0 * 60.0 * 60.0))) + " days\n"
-    time_str += str(secs % (24 * 60 * 60)) + " hours"
+    time_str += str(round((secs % (24 * 60 * 60)) / (60 * 60), 2)) + " hours"
     return time_str
 
 
-def formatTime(secs):  # one line
+def formatTimeOL(secs):  # one line
     time_str = str(math.floor(secs / (24.0 * 60.0 * 60.0))) + " days and "
-    time_str += str(secs % (24 * 60 * 60)) + " hours"
+    time_str += str(round((secs % (24 * 60 * 60)) / (60 * 60), 2)) + " hours"
     return time_str
 
 
@@ -47,23 +48,8 @@ if __name__ == "__main__":
     print("Directory: " + dirname)
     for root, dirs, filenames in os.walk(dirname):
         for f in filenames:
-            with open(os.path.join(root, f), 'r') as userFile:
-                totalTime = 0
-                lastState = "n"
-                lastTime = datetime.now()
-                for currentLine in userFile:
-                    currentLine = currentLine.strip()
-                    if currentLine:
-                        time_str = currentLine[4:]
-                        state = currentLine[0]
-                        time = datetime.strptime(time_str, ioForm)
-                        if state == "i":
-                            lastTime = time
-                        elif state == "o":
-                            if lastState == "i":
-                                totalTime += (time - lastTime).total_seconds()
-                    else:
-                        state = "n"
-                    lastState = state
-                    lastTime = time
-                print(f.split(".")[0] + ": " + str(totalTime))
+            totalTime = getSecs(os.path.join(root, f))
+            print("Total Secs: " + str(totalTime))
+            print(f.split(".")[0] + ": " + formatTimeOL(totalTime))
+            print(f.split(".")[0] + ":\n" + formatTime(totalTime))
+            print("\n")
