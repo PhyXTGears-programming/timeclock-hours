eb693c0d7eb7e273f2be5a86fd16941771f8f119
Robby Sammelson <rsamm3@gmail.com>
2017-10-20 22:14:03 -0400

    Work on integration with timeclock

--- mainHours.py
+++ mainHours.py
@@ -1,26 +1,43 @@
-from tkinter import filedialog
 import os
 from datetime import datetime
+from tkinter import filedialog
 
 ioForm = "%H:%M:%S %d.%m.%Y"
 
 
-# def calcTotalTime(n):  # returns total time in seconds
-#     total = 0
-#     iLin, oLin = '', ''
-#     prev = 'n'
-#     for line in open(opts['pathTime'] + n + '.txt'):
-#         line = line.strip()
-#         lastIOA = prev[0]
-#         currIOA = line[0]
-#
-#         if currIOA == 'i':
-#             iLin = line[4:]
-#         elif currIOA == 'o' and lastIOA != 'o' and iLin != '':
-#             oLin = line[4:]
-#             total = total + (datetime.strptime(oLin, opts['ioForm']) - datetime.strptime(iLin, opts['ioForm'])).total_seconds()
-#         prev = line
-#     return total
+def getSecs(fil):
+    with open(fil, 'r') as userFile:
+        totalTime = 0
+        lastState = "n"
+        lastTime = datetime.now()
+        for currentLine in userFile:
+            currentLine = currentLine.strip()
+            if currentLine:
+                time_str = currentLine[4:]
+                state = currentLine[0]
+                time = datetime.strptime(time_str, ioForm)
+                if state == "i":
+                    lastTime = time
+                elif state == "o":
+                    if lastState == "i":
+                        totalTime += (time - lastTime).total_seconds()
+            else:
+                state = "n"
+            lastState = state
+            lastTime = time
+        return totalTime
+
+
+def formatTime(secs):  # 2 lines
+    time_str = str(math.floor(secs / (24.0 * 60.0 * 60.0))) + " days\n"
+    time_str += str(secs % (24 * 60 * 60)) + " hours"
+    return time_str
+
+
+def formatTime(secs):  # one line
+    time_str = str(math.floor(secs / (24.0 * 60.0 * 60.0))) + " days and "
+    time_str += str(secs % (24 * 60 * 60)) + " hours"
+    return time_str
 
 
 if __name__ == "__main__":
